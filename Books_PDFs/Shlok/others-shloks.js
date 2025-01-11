63947636a55b7463346ea3b7fbf5c78a2051b76b
Helsin <rhimanshu828@gmail.com>
2025-01-11 21:19:37 +0530

    2 shlokas done, 1 remaining

--- Books_PDFs/Shlok/others-shloks.js
+++ Books_PDFs/Shlok/others-shloks.js
@@ -7,7 +7,7 @@ Galeavalambya lambitam bhujangatungamalikam
 Damad damad damaddama ninadavadamarvayam
 Chakara chandtandavam tanotu nah shivah shivam
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka1.jpg`,
   shlok2: `जटाकटा हसंभ्रम भ्रमन्निलिंपनिर्झरी विलोलवीचिवल्लरी विराजमानमूर्धनि।
 धगद्धगद्धगज्ज्वल ल्ललाटपट्टपावके किशोरचंद्रशेखरे रतिः प्रतिक्षणं मम: ॥२॥
 
@@ -16,7 +16,7 @@ Vilolavichivalarai virajamanamurdhani
 Dhagadhagadhagajjva lalalata pattapavake
 Kishora chandrashekhare ratih pratikshanam mama
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka2.webp`,
   shlok3: `धराधरेंद्रनंदिनी विलासबन्धुबन्धुर स्फुरद्दिगंतसंतति प्रमोद मानमानसे।
 कृपाकटाक्षधोरणी निरुद्धदुर्धरापदि क्वचिद्विगम्बरे मनोविनोदमेतु वस्तुनि ॥३॥
 
@@ -25,7 +25,7 @@ Sphuradigantasantati pramodamanamanase
 Krupakatakshadhorani nirudhadurdharapadi
 Kvachidigambare manovinodametuvastuni
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka3.webp`,
   shlok4: `जटाभुजंगपिंगल स्फुरत्फणामणिप्रभा कदंबकुंकुमद्रव प्रलिप्तदिग्व धूमुखे।
 मदांधसिंधु रस्फुरत्वगुत्तरीयमेदुरे मनोविनोदद्भुतं बिंभर्तुभूत भर्तरि ॥४॥
 
@@ -34,7 +34,7 @@ Kadambakunkuma dravapralipta digvadhumukhe
 Madandha sindhu rasphuratvagutariyamedure
 Mano vinodamadbhutam bibhartu bhutabhartari
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka4.jpg`,
   shlok5: `सहस्रलोचन प्रभृत्यशेषलेखशेखर प्रसूनधूलिधोरणी विधूसरां घ्रिपीठभूः।
 भुजंगराजमालया निबद्धजाटजूटकः श्रियैचिरायजायतां चकोरबंधुशेखरः ॥५॥
 
@@ -43,7 +43,7 @@ Prasuna dhulidhorani vidhusaranghripithabhuh
 Bhujangaraja malaya nibaddhajatajutaka
 Shriyai chiraya jayatam chakora bandhushekharah
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka5.webp`,
   shlok6: `ललाटचत्वरज्वल द्धनंजयस्फुलिंगभा निपीतपंच सायकंनम न्निलिंपनायकम्‌।
 सुधामयूखलेखया विराजमानशेखरं महाकपालिसंपदे शिरोजटालमस्तुनः ॥६॥
 
@@ -52,7 +52,7 @@ nipitapajnchasayakam namannilimpanayakam
 Sudha mayukha lekhaya virajamanashekharam
 Maha kapali sampade shirojatalamastunah
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka6.jpg`,
   shlok7: `करालभालपट्टिका धगद्धगद्धगज्ज्वल द्धनंजया धरीकृतप्रचंड पंचसायके।
 धराधरेंद्रनंदिनी कुचाग्रचित्रपत्र कप्रकल्पनैकशिल्पिनी त्रिलोचनेरतिर्मम ॥७॥
 
@@ -61,7 +61,7 @@ Ddhanajnjaya hutikruta prachandapajnchasayake
 Dharadharendra nandini kuchagrachitrapatraka
 Prakalpanaikashilpini trilochane ratirmama
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka7.jpg`,
   shlok8: `नवीनमेघमंडली निरुद्धदुर्धरस्फुर त्कुहुनिशीथनीतमः प्रबद्धबद्धकन्धरः।
 निलिम्पनिर्झरीधरस्तनोतु कृत्तिसिंधुरः कलानिधानबंधुरः श्रियं जगंद्धुरंधरः ॥८॥
 
@@ -70,7 +70,7 @@ Kuhu nishithinitamah prabandhabaddhakandharah
 nilimpanirjhari dharastanotu krutti sindhurah
 Kalanidhanabandhurah shriyam jagaddhurandharah
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka8.png`,
   shlok9: `प्रफुल्लनीलपंकज प्रपंचकालिमप्रभा विडंबि कंठकंध रारुचि प्रबंधकंधरम्‌।
 स्मरच्छिदं पुरच्छिंद भवच्छिदं मखच्छिदं गजच्छिदांधकच्छिदं तमंतकच्छिदं भजे ॥९॥
 
@@ -79,7 +79,7 @@ Vdambi kanthakandali raruchi prabaddhakandharam
 Smarachchidam purachchhidam bhavachchidam makhachchidam
 Gajachchidandhakachidam tamamtakachchidam bhaje
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka9.jpg`,
   shlok10: `अखर्वसर्वमंगला कलाकदम्बमंजरी रसप्रवाह माधुरी विजृंभणा मधुव्रतम्‌।
 स्मरांतकं पुरातकं भावंतकं मखांतकं गजांतकांधकांतकं तमंतकांतकं भजे ॥१०॥
 
@@ -88,7 +88,7 @@ Rasapravaha madhuri vijrumbhana madhuvratam
 Smarantakam purantakam bhavantakam makhantakam
 Gajantakandhakantakam tamantakantakam bhaje
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka10.avif`,
   shlok11: `जयत्वदभ्रविभ्रम भ्रमद्भुजंगमस्फुरद्ध गद्धगद्विनिर्गमत्कराल भाल हव्यवाट्।
 धिमिद्धिमिद्धि मिध्वनन्मृदंग तुंगमंगलध्वनिक्रमप्रवर्तित: प्रचण्ड ताण्डवः शिवः ॥११॥
 
@@ -97,7 +97,7 @@ Dhigdhigdhi nirgamatkarala bhaal havyavat
 Dhimiddhimiddhimidhva nanmrudangatungamangala
 Dhvanikramapravartita prachanda tandavah shivah
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka11.jpg`,
   shlok12: `दृषद्विचित्रतल्पयो र्भुजंगमौक्तिकमस्र जोर्गरिष्ठरत्नलोष्ठयोः सुहृद्विपक्षपक्षयोः।
 तृणारविंदचक्षुषोः प्रजामहीमहेन्द्रयोः समं प्रवर्तयन्मनः कदा सदाशिवं भजे ॥१२॥
 
@@ -106,7 +106,7 @@ Garishtharatnaloshthayoh suhrudvipakshapakshayoh
 Trushnaravindachakshushoh prajamahimahendrayoh
 Sama pravartayanmanah kada sadashivam bhaje
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka12.png`,
   shlok13: `कदा निलिंपनिर्झरी निकुंजकोटरे वसन्‌ विमुक्तदुर्मतिः सदा शिरःस्थमंजलिं वहन्‌।
 विमुक्तलोललोचनो ललामभाललग्नकः शिवेति मंत्रमुच्चरन्‌ कदा सुखी भवाम्यहम्‌ ॥१३॥
 
@@ -115,7 +115,7 @@ Vimuktadurmatih sada shirah sthamajnjalim vahanh
 Vimuktalolalochano lalamabhalalagnakah
 Shiveti mantramuchcharan sada sukhi bhavamyaham
 
-/testingHero.jpg`,
+/shloka/shiv-tandav-strota/shloka13.png`,
   shlok14: `इमं हि नित्यमेव मुक्तमुक्तमोत्तम स्तवं पठन्स्मरन्‌ ब्रुवन्नरो विशुद्धमेति संततम्‌।
 हरे गुरौ सुभक्तिमाशु याति नान्यथागतिं विमोहनं हि देहिनां सुशंकरस्य चिंतनम् ॥१६॥
 
@@ -124,7 +124,7 @@ Pathansmaran bruvannaro vishuddhimeti santatam
 Hare gurau subhaktimashu yati nanyatha gatim
 Vimohanam hi dehinam sushankarasya chintanam
 
-/images/ShivTandav01.jpg`,
+/shloka/shiv-tandav-strota/shloka14.jpg`,
 };
 
 const hanumanChalisa = {
@@ -138,147 +138,147 @@ Baranaurahubharabimalajasu Jodayakaphalachari
 Budhee-heenthanujanike Sumirowpavanakumara
 Bala-budheevidyadehoomohee Harahukaleshabikaara
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka1.jpg`,
   shlok2: `जय हनुमान ज्ञान गुन सागर। जय कपीस तिहुं लोक उजागर।।
 राम दूत अतुलित बल धामा। अंजनि-पुत्र पवनसुत नामा।।
 
 JaiHanumangyangunsagar JaiKapistihunlokujagar|| 1 ||
 Ramdootatulitbaldhama Anjaani-putraPavansutnama|| 2 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka2.jpeg`,
   shlok3: `महाबीर बिक्रम बजरंगी। कुमति निवार सुमति के संगी।।
 कंचन बरन बिराज सुबेसा। कानन कुण्डल कुँचित केसा।।
 
 MahabirBikramBajrangi KumatinivarsumatiKesangi|| 3 ||
 Kanchan baran birajsubesa KananKundalKunchitKesha|| 4 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka3.jpg`,
   shlok4: `हाथ बज्र औ ध्वजा बिराजे। कांधे मूंज जनेउ साजे।।
 शंकर सुवन केसरी नंदन। तेज प्रताप महा जग वंदन।।
 
 HathBajraAurDhvajaViraje Kaandhemoonjjaneusajai|| 5 ||
 SankarsuvankesariNandan Tejprataapmahajagbandan|| 6 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka4.jpg`,
   shlok5: `बिद्यावान गुनी अति चातुर। राम काज करिबे को आतुर।।
 प्रभु चरित्र सुनिबे को रसिया। राम लखन सीता मन बसिया।।
 
 Bidyavaanguniatichaatur Ramkajkaribekoaatur|| 7 ||
 Prabhucharitrasunibe-korasiya RamLakhanSitamanBasiya|| 8 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka5.jpg`,
   shlok6: `सूक्ष्म रूप धरि सियहिं दिखावा। बिकट रूप धरि लंक जरावा।।
 भीम रूप धरि असुर संहारे। रामचन्द्र के काज संवारे।।
 
 SukshmaroopdhariSiyahidikhava Bikatroopdharilankajarava|| 9 ||
 Bhimaroopdhariasursanhare Ramachandrakekajsanvare|| 10 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka6.jpg`,
   shlok7: `लाय सजीवन लखन जियाये। श्री रघुबीर हरषि उर लाये।।
 रघुपति कीन्ही बहुत बड़ाई। तुम मम प्रिय भरतहि सम भाई।।
 
 LayeSanjivanLakhanJiyaye ShriRaghubirHarashiurlaye|| 11 ||
 RaghupatiKinhibahutbadai TummampriyBharat-hi-sambhai|| 12 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka7.jpg`,
   shlok8: `सहस बदन तुम्हरो जस गावैं। अस कहि श्रीपति कण्ठ लगावैं।।
 सनकादिक ब्रह्मादि मुनीसा। नारद सारद सहित अहीसा।।
 
 Sahasbadantumharojasgaavai Asa-kahiShripatikanthlagaavai|| 13 ||
 SanakadhikBrahmaadiMuneesa Narad-SaradsahitAheesa|| 14 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka8.jpg`,
   shlok9: `जम कुबेर दिगपाल जहां ते। कबि कोबिद कहि सके कहां ते।।
 तुम उपकार सुग्रीवहिं कीन्हा। राम मिलाय राज पद दीन्हा।।
 
 JamaKuberDigpaalJahante Kavikovidkahisakekahante|| 15 ||
 TumupkarSugreevahinkeenha Rammilayerajpaddeenha|| 16 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka9.jpg`,
   shlok10: `तुम्हरो मंत्र बिभीषन माना। लंकेश्वर भए सब जग जाना।।
 जुग सहस्र जोजन पर भानु। लील्यो ताहि मधुर फल जानू।।
 
 TumharomantraBibheeshanmaana LankeshwarBhayeSub jagjana|| 17 ||
 JugsahasrajojanparBhanu Leelyotahimadhurphaljanu|| 18 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka10.jpg`,
   shlok11: `प्रभु मुद्रिका मेलि मुख माहीं। जलधि लांघि गये अचरज नाहीं।।
 दुर्गम काज जगत के जेते। सुगम अनुग्रह तुम्हरे तेते।।
 
 Prabhumudrikamelimukhmahee Jaladhilanghigayeachrajnahee|| 19 ||
 Durgaamkajjagathkejete Sugamanugrahatumhretete|| 20 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka11.jpg`,
   shlok12: `राम दुआरे तुम रखवारे। होत न आज्ञा बिनु पैसारे।।
 सब सुख लहै तुम्हारी सरना। तुम रच्छक काहू को डर ना।।
 
 Ramduaaretumrakhvare Hoatnaagyabinupaisare|| 21 ||
 Subsukhlahaetumharisarna Tumracchakkahukodarnaa|| 22 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka12.jpg`,
   shlok13: `आपन तेज सम्हारो आपै। तीनों लोक हांक तें कांपै।।
 भूत पिसाच निकट नहिं आवै। महाबीर जब नाम सुनावै।।
 
 Aapantejsamharoaapai Teenhonlokhanktekanpai|| 23 ||
 BhootpisaachNikatnahinaavai Mahavirjabnaamsunavae|| 24 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka13.jpg`,
   shlok14: `नासै रोग हरे सब पीरा। जपत निरन्तर हनुमत बीरा।।
 संकट तें हनुमान छुड़ावै। मन क्रम बचन ध्यान जो लावै।।
 
 Nasaerogharaesabpeera JapatnirantarHanumantbeera|| 25 ||
 SankatseHanumanchudavae ManKrambachandhyanjolavai|| 26 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka14.jpg`,
   shlok15: `सब पर राम तपस्वी राजा। तिन के काज सकल तुम साजा।।
 और मनोरथ जो कोई लावै। सोई अमित जीवन फल पावै।।
 
 Sabpar Ramtapasveeraja TinkekajsakalTumsaja|| 27 ||
 Aurmanorathjokoilavai Sohiamitjeevanphalpavai|| 28 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka15.jpg`,
   shlok16: `चारों जुग परताप तुम्हारा। है परसिद्ध जगत उजियारा।।
 साधु संत के तुम रखवारे।। असुर निकन्दन राम दुलारे।
 
 Charonjugpartaptumhara Haipersidhjagatujiyara|| 29 ||
 SadhuSantketumRakhware AsurnikandanRamdulhare|| 30 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka16.jpg`,
   shlok17: `अष्टसिद्धि नौ निधि के दाता। अस बर दीन जानकी माता।।
 राम रसायन तुम्हरे पासा। सदा रहो रघुपति के दासा।।
 
 Ashta-sidhinavnidhikedata As-baradeenJanakimata|| 31 ||
 Ramrasayantumharepasa SadarahoRaghupatikedasa|| 32 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka17.jpg`,
   shlok18: `तुह्मरे भजन राम को पावै। जनम जनम के दुख बिसरावै।।
 अंत काल रघुबर पुर जाई। जहां जन्म हरिभक्त कहाई।।
 
 TumharebhajanRamkopavai Janam-janamkedukhbisraavai|| 33 ||
 Anth-kaalRaghubarpurjayee JahanjanamHari-BakhtKahayee|| 34 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka18.jpg`,
   shlok19: `और देवता चित्त न धरई। हनुमत सेइ सर्ब सुख करई।।
 सङ्कट कटै मिटै सब पीरा। जो सुमिरै हनुमत बलबीरा।।
 
 AurDevtaChitnadharehi Hanumatse hisarvesukhkarehi|| 35 ||
 Sankatkate-mitesabpeera JosumiraiHanumatBalbeera|| 36 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka19.jpg`,
   shlok20: `जय जय जय हनुमान गोसाईं। कृपा करहु गुरुदेव की नाईं।।
 जो सत बार पाठ कर कोई। छूटहि बन्दि महा सुख होई।।
 
 Jai Jai JaiHanumanGosain KripaKarahuGurudevkinain|| 37 ||
 Jo sat bar pathkarekohi Chutahibandhimahasukhhohi|| 38 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka20.jpeg`,
   shlok21: `  जो यह पढ़ै हनुमान चालीसा। होय सिद्धि साखी गौरीसा।।
 तुलसीदास सदा हरि चेरा। कीजै नाथ हृदय महं डेरा।।
 
 Jo yahpadhaeHanumanChalisa HoyesiddhisaakhiGaureesa|| 39 ||
 Tulsidassadaharichera KeejaiNathHridayemaheindera|| 40 ||
 
-/testingHero.jpg`,
+/shloka/hanuman-chalisa/shloka21.jpg`,
 };
 
 const krishnaKiChetavani = {
